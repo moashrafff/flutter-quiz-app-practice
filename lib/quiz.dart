8f0ef3704f6d6a0fd46c70f95c6e73929ebf9238
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2024-10-12 15:47:02 +0300

    using regural if statement

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -11,28 +11,32 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
 
- String activeScreen = 'start-screen';
-
-void switchScreen(){
-  setState(() {
-    activeScreen = 'question-screen';
-  });
-}
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'question-screen';
+    });
+  }
 
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'question-screen')
+      screenWidget = const QuestionsScreen();
+
     return MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 78, 13, 151),
-          Color.fromARGB(255, 107, 15, 168)
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-        child: activeScreen == 'start-screen' ? StartScreen(switchScreen) : const QuestionsScreen(),
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 78, 13, 151),
+            Color.fromARGB(255, 107, 15, 168)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: screenWidget,
+        ),
       ),
-    ),
-  );
+    );
   }
 }
